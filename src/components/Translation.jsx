fd207920510177aaea9ecb61423e8ea5635c567f
ben <ben.lee8602@gmail.com>
2024-04-03 23:48:10 -0400

    implement translation

--- src/components/Translation.jsx
+++ src/components/Translation.jsx
@@ -2,7 +2,18 @@ import Glasses from "./Glasses";
 import "../styles/Translation.css";
 
 function Translation() {
-    return (<Glasses bgUrl="translatebg.png" className="translation">
+    return (<Glasses bgUrl="translatebg.png">
+        <div className="translation">
+            <div className="target" id="t1">
+                <div className="target-text">English text</div>
+            </div>
+            <div className="target" id="t2">
+                <div className="target-text">English text</div>
+            </div>
+            <div className="target" id="t3">
+                <div className="target-text">English text</div>
+            </div>
+        </div>
     </Glasses>);
 }
 
