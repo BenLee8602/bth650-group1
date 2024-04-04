5bdcb3777484c2e14132019f65d18180e3cb4847
ben <ben.lee8602@gmail.com>
2024-04-03 23:04:39 -0400

    fix backgrounds

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -1,18 +1,19 @@
 import React from "react";
+import Glasses from "./Glasses.jsx";
 import "../style/Notifications.css";
 import { DirectionPopup } from "./sub/directionPop";
 import { NotificationPopup } from "./sub/msgPop";
 
 function Notifications() {
     return (
-        <>
+        <Glasses bgUrl="./src/assets/images/italy.jpg">
             <div className={'baseContainer'}>
                 <div className={'column'}>
                     <DirectionPopup property1="default" className="class" />
                     <NotificationPopup />
                 </div>
             </div>
-        </>
+        </Glasses>
     );
 }
 
